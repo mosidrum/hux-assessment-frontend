3ba2029a8b3f6a67f5e77b6ce9123982e7179b6b
mosidrum <mosiokanga@gmail.com>
2023-11-10 04:29:37 +0100

    Send registration data to backend

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,19 +1,28 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
+import paths from '../routes/paths';
 
-const Login = () => (
-  <div>
-    <form>
-      <div>
-        <label htmlFor="email">Email</label>
-        <input type="email" name="email" id="email" />
-      </div>
-      <div>
-        <label htmlFor="password">Password</label>
-        <input type="password" name="password" id="password" />
-      </div>
-      <button type="submit" className="btn btn-primary">Login</button>
-    </form>
-  </div>
-);
+const Login = () => {
+  const navigate = useNavigate();
+
+  return (
+    <div>
+      <form>
+        <div>
+          <label htmlFor="email">Email</label>
+          <input type="email" name="email" id="email" />
+        </div>
+        <div>
+          <label htmlFor="password">Password</label>
+          <input type="password" name="password" id="password" />
+        </div>
+        <button type="submit" className="btn btn-primary">Login</button>
+        <div>
+          <button type="button" onClick={() => navigate(paths.signup)}>CLick here to Signup</button>
+        </div>
+      </form>
+    </div>
+  );
+};
 
 export default Login;
--- src/components/SignUp.jsx
+++ src/components/SignUp.jsx
@@ -1,21 +1,49 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import axios from 'axios';
+import paths from '../routes/paths';
 
-const SignUp = () => (
-  <form>
-    <div>
-      <label htmlFor="name">Name</label>
-      <input type="name" name="name" id="name" />
-    </div>
-    <div>
-      <label htmlFor="email">Email</label>
-      <input type="email" name="email" id="email" />
-    </div>
-    <div>
-      <label htmlFor="password">Password</label>
-      <input type="password" name="password" id="password" />
-    </div>
-    <button type="submit" className="btn btn-primary">Sign Up</button>
-  </form>
-);
+const SignUp = () => {
+  const navigate = useNavigate();
+  const [values, setValues] = useState({
+    name: '',
+    email: '',
+    password: '',
+  });
+
+  const handleChange = (e) => {
+    setValues({
+      ...values, [e.target.name]: e.target.value,
+    });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    axios.post('http://localhost:8081/register', values)
+      .then((res) => console.log(res))
+      .then((err) => console.log(err));
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <div>
+        <label htmlFor="name">Name</label>
+        <input type="name" name="name" id="name" onChange={handleChange} />
+      </div>
+      <div>
+        <label htmlFor="email">Email</label>
+        <input type="email" name="email" id="email" onChange={handleChange} />
+      </div>
+      <div>
+        <label htmlFor="password">Password</label>
+        <input type="password" name="password" id="password" onChange={handleChange} />
+      </div>
+      <button type="submit">Sign Up</button>
+      <div>
+        <button type="button" onClick={() => navigate(paths.login)}>CLick here to Login</button>
+      </div>
+    </form>
+  );
+};
 
 export default SignUp;
